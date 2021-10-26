745936a729771d6ab31d97449e7843153293d932
Andy Kitchen <kitchen.andy+git@gmail.com>
2021-10-27 08:19:28 +1100

    improved simple frequency counter

--- rtl/freq_counter.py
+++ rtl/freq_counter.py
@@ -8,19 +8,21 @@ class FrequencyCounter(Module, AutoCSR, AutoDoc):
     """Frequency counter"""
     def __init__(self, pads):
         self.elapse = CSRStatus(32, name="elapse", description="""number of clock ticks elapsed""")
-        self.cycles = CSRStatus(16, name="cycles", description="""number of signal cycles counted""")
+        self.cycles = CSRStatus(32, name="cycles", description="""number of signal cycles counted""")
 
-        self.reset = CSRStorage(1, name="reset", fields = [
-            CSRField("reset", size=1, description="Write `1` for a synchronous reset of the counters", pulse=True)
+        self.ctrl = CSRStorage(1, name="ctrl", fields = [
+            CSRField("reset", size=1, description="Write `1` for a synchronous reset of the counters", pulse=True),
+            CSRField("disable", size=1, description="Write `1` to disable counter")
         ])
 
         self.intro = ModuleDoc("""
         Simple frequency counter
         """)
 
-        elapse = self.elapse.status
-        cycles = self.cycles.status
-        reset  = self.reset.fields.reset
+        elapse  = self.elapse.status
+        cycles  = self.cycles.status
+        reset   = self.ctrl.fields.reset
+        disable = self.ctrl.fields.disable
 
         elapse_next = Signal(len(elapse))
         cycles_next = Signal(len(cycles))
@@ -36,13 +38,14 @@ class FrequencyCounter(Module, AutoCSR, AutoDoc):
         ]
 
         self.sync += [
-            If(reset,
-                elapse.eq(0),
-                cycles.eq(0),
-            ).Else(
-                sig_prev.eq(sig),
-                elapse.eq(elapse_next),
-                If(elapse_next == Constant(0, len(elapse)), cycles.eq(0))
-                  .Else(If(sig & ~sig_prev, cycles.eq(cycles_next))),
-            ),
+            If(~disable,
+                If(reset,
+                    elapse.eq(0),
+                    cycles.eq(0),
+                ).Else(
+                    sig_prev.eq(sig),
+                    elapse.eq(elapse_next),
+                    If(elapse_next == Constant(0, len(elapse)), cycles.eq(0))
+                      .Else(If(sig & ~sig_prev, cycles.eq(cycles_next))),
+                ))
         ]
--- tools/read_freq.py
+++ tools/read_freq.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python3
 
+import time
 from litex import RemoteClient
 
 wb = RemoteClient()
 wb.open()
+
+# reset frequency counters
+wb.regs.freq_cnt0_ctrl.write(0x1)
+wb.regs.freq_cnt1_ctrl.write(0x1)
+
+# collect some cycles
+time.sleep(1.0)
+
+# pause frequency counters
+wb.regs.freq_cnt0_ctrl.write(0x2)
+wb.regs.freq_cnt1_ctrl.write(0x2)
 
 def get_freq(reg_elapse, reg_cycles):
 	elapse = reg_elapse.read()
 	cycles = reg_cycles.read()
 	elapse_seconds = elapse / 12e6
 	freq = cycles / elapse_seconds
-	return freq
+	return freq, cycles, elapse_seconds
 
-f0 = get_freq(wb.regs.freq_cnt0_elapse, wb.regs.freq_cnt0_cycles)
-f1 = get_freq(wb.regs.freq_cnt1_elapse, wb.regs.freq_cnt1_cycles)
+f0, n0, t0 = get_freq(wb.regs.freq_cnt0_elapse, wb.regs.freq_cnt0_cycles)
+f1, n1, t1 = get_freq(wb.regs.freq_cnt1_elapse, wb.regs.freq_cnt1_cycles)
 
-print("osc0: {:.2f} Htz".format(f0))
-print("osc1: {:.2f} Htz".format(f1))
+print("osc0: {:8.2f} Htz  ({:4.0f} / {:4.0f}ms)".format(f0, n0, 1000*t0))
+print("osc1: {:8.2f} Htz  ({:4.0f} / {:4.0f}ms)".format(f1, n1, 1000*t1))
+
+# reset frequency counters again
+wb.regs.freq_cnt0_ctrl.write(0x1)
+wb.regs.freq_cnt1_ctrl.write(0x1)
 
 wb.close()
