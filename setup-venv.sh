242877bec3d7cfa74134ae1f7777e710243e7e35
Andy Kitchen <kitchen.andy+git@gmail.com>
2021-11-10 11:13:31 +1100

    added venv setup script

---
+++ setup-venv.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PYTHON=python3
+VENV=venv
+
+if [ ! -e venv ]; then
+	$PYTHON -m venv $VENV
+fi
+
+source $VENV/bin/activate || exit 1
+
+#pip install -r requirements.txt
+
+find deps -maxdepth 2 -name "setup.py" | while read line; do
+	(
+		echo install "$line"
+		cd `dirname "$line"` &&
+		pip install -e .
+	)
+done
