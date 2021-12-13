bf3ba0c1c917c6ed8532de48c36321eaaf370328
Andy Kitchen <kitchen.andy+git@gmail.com>
2021-12-13 18:38:34 +1100

    improved clock resource generator
    For some reason the old design did not actually check if
    the PLL was locked before bringing the system out of reset,
    I've added this feature and cleaned up the overall Power on Reset
    logic

--- rtl/crg.py
+++ rtl/crg.py
@@ -1,5 +1,5 @@
 from migen import Module, Signal, Instance, ClockDomain, If
-from migen.genlib.cdc import AsyncResetSynchronizer
+from migen.genlib.cdc import AsyncResetSynchronizer, MultiReg
 
 from litex.soc.integration.doc import AutoDoc
 
@@ -31,50 +31,83 @@ class FomuClockResourceGenerator(Module, AutoDoc):
     +---------+------------+---------------------------------+
     """
     def __init__(self, platform):
-        clk48_raw = platform.request("clk48")
-        clk12 = Signal()
-
-        clk48_gb = Signal()
-
-        # Put clock signal into a global buffer
-        self.specials += Instance(
-            "SB_GB_IO",
-            i_PACKAGE_PIN = clk48_raw,
-            o_GLOBAL_BUFFER_OUTPUT = clk48_gb
-        )
-
-        reset_delay = Signal(12, reset=4095)
-        self.clock_domains.cd_por = ClockDomain()
-        self.reset = Signal()
-
-        # Certain internal parts of LiteX expect reset to be called `rst'
-        self.rst = self.reset
-
-        self.clock_domains.cd_sys = ClockDomain()
+        self.clock_domains.cd_sys    = ClockDomain()
+        self.clock_domains.cd_por    = ClockDomain()
         self.clock_domains.cd_usb_12 = ClockDomain()
         self.clock_domains.cd_usb_48 = ClockDomain()
 
-        platform.add_period_constraint(self.cd_usb_48.clk, 1e9/48e6)
-        platform.add_period_constraint(self.cd_sys.clk, 1e9/12e6)
+        clk48 = Signal() # Output by global buffer
+        clk12 = Signal() # Output by PLL see below
+
+        clk48_raw = platform.request("clk48")
+
+        # Manually place 48MHz clock signal into a global buffer
+        self.specials += Instance(
+            "SB_GB_IO",
+            i_PACKAGE_PIN = clk48_raw,
+            o_GLOBAL_BUFFER_OUTPUT = clk48
+        )
+
+        platform.add_period_constraint(self.cd_sys.clk,    1e9/12e6)
         platform.add_period_constraint(self.cd_usb_12.clk, 1e9/12e6)
-        platform.add_period_constraint(clk48_gb, 1e9/48e6)
+        platform.add_period_constraint(self.cd_usb_48.clk, 1e9/48e6)
+        platform.add_period_constraint(clk48,              1e9/48e6)
 
-        # POR reset logic- POR generated from sys clk, POR logic feeds sys clk
-        # reset.
+        # Power on Reset (POR) delay logic
+        # Hold entire system in reset during countdown or when PLL is not yet locked
+        # The countdown is there because apparently early on the lock signal can be unreliable,
+        # so we ignore the PLL lock signal until it has had a chance to stabilise
+        reset_delay_bits = 12
+        reset_delay = Signal(reset_delay_bits, reset=2**reset_delay_bits-1)
+        reset_delay_zero = Signal()
+        pll_lock = Signal()
+        por_reset = Signal()
+        por_reset_raw = (~reset_delay_zero | ~pll_lock)
+        por_reset_sync = Signal()
+
+        # NB the PLL lock signal may be async, but we want our global
+        # reset signal to be synchronous so we use a synchroniser here,
+        # this also has ths side effect of slightly delaying internally triggered
+        # whole system resets by two clock cycles
+        self.specials += MultiReg(i=por_reset_raw, o=por_reset_sync, n=2, odomain='por')
+
+        # Manually place POR reset into global buffer
+        self.specials += Instance(
+            "SB_GB",
+            i_USER_SIGNAL_TO_GLOBAL_BUFFER = por_reset_sync,
+            o_GLOBAL_BUFFER_OUTPUT = por_reset
+        )
+
         self.comb += [
-            self.cd_por.clk.eq(self.cd_sys.clk),
-            self.cd_sys.rst.eq(reset_delay != 0),
-            self.cd_usb_12.rst.eq(reset_delay != 0),
+            reset_delay_zero.eq(reset_delay == 0),
         ]
 
-        # POR reset logic- POR generated from sys clk, POR logic feeds sys clk
-        # reset.
-        self.comb += [
-            self.cd_usb_48.rst.eq(reset_delay != 0),
+        self.sync.por += [
+            If(~reset_delay_zero,
+                reset_delay.eq(reset_delay - 1)),
         ]
 
-        self.comb += self.cd_usb_48.clk.eq(clk48_gb)
+        # Allow Power on Reset system to be internally triggered, this is the whole system reset line
+        # This seems to ruin the USB connection though, perhaps some bad interaction with resetting and USB devices.
+        self.reset = Signal()
+        self.specials += AsyncResetSynchronizer(self.cd_por, self.reset)
 
+        self.rst = self.reset   # Certain internal parts of LiteX expect reset to be called `rst'
+
+        # Wire up all clock domains
+        self.comb += [
+            self.cd_por.clk.eq(clk12),
+
+            self.cd_sys.clk.eq(clk12),
+            self.cd_usb_12.clk.eq(clk12),
+            self.cd_usb_48.clk.eq(clk48),
+
+            self.cd_sys.rst.eq(por_reset),
+            self.cd_usb_12.rst.eq(por_reset),
+            self.cd_usb_48.rst.eq(por_reset),
+        ]
+
+        # Setup up iCE40UP5k internal PLL
         self.specials += Instance(
             "SB_PLL40_CORE",
             # Parameters for 12MHz
@@ -91,24 +124,15 @@ class FomuClockResourceGenerator(Module, AutoDoc):
             p_PLLOUT_SELECT = "GENCLK_HALF",
             p_ENABLE_ICEGATE = 0,
             # IO
-            i_REFERENCECLK = clk48_gb,
+            i_REFERENCECLK = clk48,
             #o_PLLOUTCORE = clk12,
             o_PLLOUTGLOBAL = clk12,
             #i_EXTFEEDBACK,
             #i_DYNAMICDELAY,
-            #o_LOCK,
+            o_LOCK = pll_lock,
             i_BYPASS = 0,
             i_RESETB = 1,
             #i_LATCHINPUTVALUE,
             #o_SDO,
             #i_SDI,
         )
-
-        self.comb += self.cd_sys.clk.eq(clk12)
-        self.comb += self.cd_usb_12.clk.eq(clk12)
-
-        self.sync.por += \
-            If(reset_delay != 0,
-                reset_delay.eq(reset_delay - 1)
-            )
-        self.specials += AsyncResetSynchronizer(self.cd_por, self.reset)
