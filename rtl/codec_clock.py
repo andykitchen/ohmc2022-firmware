fb227d97e48b29e5d7b1e788aec70274f8095d35
Andy Gelme <geekscape@gmail.com>
2022-02-05 17:00:01 +1100

    Fix "custom-bios/codec_reg.h" CHIP_ANA_POWER register fields: STARTUP_POWERUP should be bit 12 and LINREG_SIMPLE_POWERUP should be bit 13.  Also, provide additional CHIP_ANA_POWER register fields with details.  Provided some additional notes in "doc/codec.md".

--- rtl/codec_clock.py
+++ rtl/codec_clock.py
@@ -2,7 +2,7 @@ from migen import Module
 from migen.fhdl.structure import ClockSignal
 
 class CodecClock(Module):
-    """Provice a master clock signal for the SGTL5000 audio codec"""
+    """Provide a master clock signal for the SGTL5000 audio codec"""
     def __init__(self, pads):
         clk = ClockSignal()
         self.comb += [
